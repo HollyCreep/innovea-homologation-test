a889a8f308c59fa49857ed51d6158a88d41f5ee1
Lucas Ferreira <lucasferreirarm@gmail.com>
2023-07-12 16:04:32 -0300

    Removing unused view

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,11 +9,6 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/pagination',
-      name: 'pagination',
-      component: () => import('../views/PaginationView.vue'),
-    },
   ],
 })
 
